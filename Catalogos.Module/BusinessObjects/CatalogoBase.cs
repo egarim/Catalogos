0af674d0ed1ec8d3714cc43f361232033f7119aa
Jose Ojeda <joche.ojeda@bitframeworks.com>
2022-12-08 01:50:45 +0300

    Add project files.

---
+++ Catalogos.Module/BusinessObjects/CatalogoBase.cs
@@ -0,0 +1,70 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.DC;
+using DevExpress.ExpressApp.Model;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.Validation;
+using DevExpress.Xpo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace Catalogos.Module.BusinessObjects
+{
+    [DefaultClassOptions]
+    
+    //[ImageName("BO_Contact")]
+    [DefaultProperty("Nombre")]
+    //[DefaultListViewOptions(MasterDetailMode.ListViewOnly, false, NewItemRowPosition.None)]
+    //[Persistent("DatabaseTableName")]
+    // Specify more UI options using a declarative approach (https://documentation.devexpress.com/#eXpressAppFramework/CustomDocument112701).
+    public class CatalogoBase : BaseObject
+    { // Inherit from a different class to provide a custom primary key, concurrency and deletion behavior, etc. (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument113146.aspx).
+        // Use CodeRush to create XPO classes and properties with a few keystrokes.
+        // https://docs.devexpress.com/CodeRushForRoslyn/118557
+        public CatalogoBase(Session session)
+            : base(session)
+        {
+        }
+        public override void AfterConstruction()
+        {
+            base.AfterConstruction();
+            // Place your initialization code here (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument112834.aspx).
+        }
+
+        bool activo;
+        string descripcion;
+        string nombre;
+        string codigo;
+
+        [Size(SizeAttribute.DefaultStringMappingFieldSize)]
+        public string Codigo
+        {
+            get => codigo;
+            set => SetPropertyValue(nameof(Codigo), ref codigo, value);
+        }
+
+        [Size(SizeAttribute.DefaultStringMappingFieldSize)]
+        public string Nombre
+        {
+            get => nombre;
+            set => SetPropertyValue(nameof(Nombre), ref nombre, value);
+        }
+
+        [Size(SizeAttribute.DefaultStringMappingFieldSize)]
+        public string Descripcion
+        {
+            get => descripcion;
+            set => SetPropertyValue(nameof(Descripcion), ref descripcion, value);
+        }
+        
+        public bool Activo
+        {
+            get => activo;
+            set => SetPropertyValue(nameof(Activo), ref activo, value);
+        }
+    }
+}
---
+++ Catalogos.Module/BusinessObjects/Prueba.cs
@@ -0,0 +1,70 @@
+﻿using DevExpress.Data.Filtering;
+using DevExpress.ExpressApp;
+using DevExpress.ExpressApp.DC;
+using DevExpress.ExpressApp.Model;
+using DevExpress.Persistent.Base;
+using DevExpress.Persistent.BaseImpl;
+using DevExpress.Persistent.Validation;
+using DevExpress.Xpo;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+
+namespace Catalogos.Module.BusinessObjects
+{
+    [DefaultClassOptions]
+    //[ImageName("BO_Contact")]
+    //[DefaultProperty("DisplayMemberNameForLookupEditorsOfThisType")]
+    //[DefaultListViewOptions(MasterDetailMode.ListViewOnly, false, NewItemRowPosition.None)]
+    //[Persistent("DatabaseTableName")]
+    // Specify more UI options using a declarative approach (https://documentation.devexpress.com/#eXpressAppFramework/CustomDocument112701).
+    public class Prueba : BaseObject
+    { // Inherit from a different class to provide a custom primary key, concurrency and deletion behavior, etc. (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument113146.aspx).
+        // Use CodeRush to create XPO classes and properties with a few keystrokes.
+        // https://docs.devexpress.com/CodeRushForRoslyn/118557
+        public Prueba(Session session)
+            : base(session)
+        {
+        }
+        public override void AfterConstruction()
+        {
+            base.AfterConstruction();
+            // Place your initialization code here (https://documentation.devexpress.com/eXpressAppFramework/CustomDocument112834.aspx).
+        }
+
+        string test;
+        CategoriaProducto categoriaProducto;
+        CategoriaCliente cliente;
+
+        public CategoriaCliente Cliente
+        {
+            get => cliente;
+            set => SetPropertyValue(nameof(Cliente), ref cliente, value);
+        }
+
+        public CategoriaProducto CategoriaProducto
+        {
+            get => categoriaProducto;
+            set => SetPropertyValue(nameof(CategoriaProducto), ref categoriaProducto, value);
+        }
+        
+        [Size(SizeAttribute.DefaultStringMappingFieldSize)]
+        public string Test
+        {
+            get => test;
+            set => SetPropertyValue(nameof(Test), ref test, value);
+        }
+
+        [Delayed(), VisibleInListViewAttribute(true)]
+        [RuleRequiredField]
+        [ImageEditor(ListViewImageEditorMode = ImageEditorMode.PopupPictureEdit, DetailViewImageEditorMode = ImageEditorMode.DropDownPictureEdit, ListViewImageEditorCustomHeight = 40)]
+        public byte[] Foto
+        {
+            get { return GetDelayedPropertyValue<byte[]>(nameof(Foto)); }
+            set { SetDelayedPropertyValue<byte[]>(nameof(Foto), value); }
+        }
+
+    }
+}
